eea834556d1ccaea6c96d67fff003b5ee70b7285
Triệu Tuyên Nhâm <trieunham2002@gmail.com>
2023-04-27 12:49:03 +0700

    using function compnent

--- frontend/src/components/Home/DarkMode/MessageItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "../../../style/global/Message.scss";
-
-export default class MessageItem extends React.Component {
-    render() {
-        return <div className="message-item-image"></div>;
-    }
-}
---
+++ frontend/src/components/Home/DarkMode/TopbarNavigation/Conversation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Conversation() {
+    return (
+        <>
+            <div className="nav-search">
+                <button className="nav-search-input">
+                    Find or start a conversation
+                </button>
+            </div>
+        </>
+    );
+}
