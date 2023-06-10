93943f57abc6bfdd06463110b480463276aa3d95
Triệu Tuyên Nhâm <trieunham2002@gmail.com>
2023-06-10 21:54:13 +0700

    giao dien dang nhap, dang ky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ReactDOM } from "react";
+import Login from "./components/Home/DarkMode/login/Login";
 
 import "./App.scss";
 import DarkMode from "./components/Home/DarkMode/DarkMode";
+import Signup from "./components/Home/DarkMode/signup/Signup";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <DarkMode />
+                {/* <DarkMode /> */}
+                {/* <Login /> */}
+                <Signup />
             </div>
         </BrowserRouter>
     );
--- frontend/src/components/Home/DarkMode/login/Login.tsx
+++ frontend/src/components/Home/DarkMode/login/Login.tsx
@@ -1,5 +1,58 @@
+import loginBackground from "../../../../assets/image/login/login.png";
+import "./Login.scss";
 const Login = () => {
-    return <></>;
+    return (
+        <div className="login-container">
+            <div className="main-login">
+                <div className="background">
+                    <img src={loginBackground} alt="" />
+                    <div className="wrap-login-form">
+                        <form className="login-form">
+                            <div className="title">
+                                <h1>Chao mung tro lai</h1>
+                                <span>Rat vui mung khi duoc gap lai ban</span>
+                            </div>
+                            <div className="user">
+                                <div className="group">
+                                    <label htmlFor="login-email">
+                                        Email hoac so dien thoai<span>*</span>
+                                    </label>
+                                    <input type="text" id="login-email" />
+                                </div>
+                                <div className="group">
+                                    <label htmlFor="password">
+                                        Mat khau<span>*</span>
+                                    </label>
+                                    <input type="text" id="password" />
+                                </div>
+                                <button
+                                    type="button"
+                                    className="forgot-password"
+                                >
+                                    Quen mat khau?
+                                </button>
+                                <button type="submit" className="login-button">
+                                    Dang nhap
+                                </button>
+                                <div className="need-account">
+                                    <span>Can mot tai khoan? </span>
+                                    <button type="button">Dang ky</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div className="qr-code">
+                            <div className="qr"></div>
+                            <h1 className="title">Dang nhap bang Ma QR</h1>
+                            <p>
+                                Quet bang <b>ung dung di dong Discord</b> de
+                                dang nhap tuc thi.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
