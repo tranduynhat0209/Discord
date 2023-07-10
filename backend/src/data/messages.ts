83f6276d7254fb6bc69b98aaf80b7e2febffdb6e
Triệu Tuyên Nhâm <trieunham2002@gmail.com>
2023-07-10 21:45:28 +0700

    update: upload img file

--- backend/src/data/messages.ts
+++ backend/src/data/messages.ts
@@ -4,64 +4,66 @@ import { Message as MessageEntity } from "./entity/Message";
 import { generateSnowflake } from "./snowflake-entity";
 
 export default class Messages extends DBWrapper<string, MessageEntity> {
-  public async get(id: string | undefined) {
-    const message = await deps.dataSource.manager.findOneBy(MessageEntity, {
-      id,
-    });
-    if (!message) throw new TypeError("Message doesn't exist");
-    return message;
-  }
-
-  public async create(
-    authorId: string,
-    channelId: string,
-    { attachmentURLs, content }: Partial<Entity.Message>
-  ) {
-    if (!content && !attachmentURLs?.length)
-      throw new TypeError("Empty messages are not valid");
-    attachmentURLs = attachmentURLs || [];
-
-    const id = generateSnowflake();
-    await deps.dataSource.manager.save(MessageEntity, {
-      id,
-      attachmentURLs,
-      authorId,
-      channelId,
-      content,
-    });
-
-    return await this.get(id);
-  }
-
-  public async createSystem(
-    guildId: string,
-    content: string,
-    type?: MessageTypes.Type
-  ) {
-    const guild = await deps.guilds.get(guildId);
-    if (!guild) {
-      throw new TypeError("Guild not found");
+    public async get(id: string | undefined) {
+        const message = await deps.dataSource.manager.findOneBy(MessageEntity, {
+            id,
+        });
+        if (!message) throw new TypeError("Message doesn't exist");
+        return message;
     }
-    if (!guild.systemChannelId)
-      throw new TypeError("No system channel configured");
 
-    return await deps.dataSource.manager.save(MessageEntity, {
-      id: generateSnowflake(),
-      type,
-      channelId: guild.systemChannelId,
-      content,
-      system: true,
-    });
-  }
+    public async create(
+        authorId: string,
+        channelId: string,
+        { attachmentURLs, content }: Partial<Entity.Message>
+    ) {
+        if (!content && !attachmentURLs?.length)
+            throw new TypeError("Empty messages are not valid");
+        // attachmentURLs = attachmentURLs || [];
 
-  public async getChannelMessages(channelId: string) {
-    return await deps.dataSource.manager.findBy(MessageEntity, { channelId });
-  }
+        const id = generateSnowflake();
+        await deps.dataSource.manager.save(MessageEntity, {
+            id,
+            attachmentURL: attachmentURLs,
+            authorId,
+            channelId,
+            content,
+        });
 
-  public async getDMChannelMessages(userId0: string, userId1: string) {
-    const channel = await deps.dmChannels.getByUserIds(userId0, userId1);
-    if (!channel) return new TypeError("Channel doesn't exist");
+        return await this.get(id);
+    }
 
-    return await this.getChannelMessages(channel.id);
-  }
+    public async createSystem(
+        guildId: string,
+        content: string,
+        type?: MessageTypes.Type
+    ) {
+        const guild = await deps.guilds.get(guildId);
+        if (!guild) {
+            throw new TypeError("Guild not found");
+        }
+        if (!guild.systemChannelId)
+            throw new TypeError("No system channel configured");
+
+        return await deps.dataSource.manager.save(MessageEntity, {
+            id: generateSnowflake(),
+            type,
+            channelId: guild.systemChannelId,
+            content,
+            system: true,
+        });
+    }
+
+    public async getChannelMessages(channelId: string) {
+        return await deps.dataSource.manager.findBy(MessageEntity, {
+            channelId,
+        });
+    }
+
+    public async getDMChannelMessages(userId0: string, userId1: string) {
+        const channel = await deps.dmChannels.getByUserIds(userId0, userId1);
+        if (!channel) return new TypeError("Channel doesn't exist");
+
+        return await this.getChannelMessages(channel.id);
+    }
 }
--- backend/src/ws/ws-events/message-create.ts
+++ backend/src/ws/ws-events/message-create.ts
@@ -4,37 +4,38 @@ import { WSEvent } from ".";
 import { Entity, WS } from "../../types";
 
 export default class implements WSEvent<"MESSAGE_CREATE"> {
-  on = "MESSAGE_CREATE" as const;
+    on = "MESSAGE_CREATE" as const;
 
-  public async invoke(
-    ws: WebSocket,
-    client: Socket,
-    { attachmentURLs, channelId, content, embed }: WS.Params.MessageCreate
-  ) {
-    const authorId = ws.sessions.userId(client);
+    public async invoke(
+        ws: WebSocket,
+        client: Socket,
+        { attachmentURLs, channelId, content, embed }: WS.Params.MessageCreate
+    ) {
+        const authorId = ws.sessions.userId(client);
 
-    const [channel, author] = await Promise.all([
-      deps.channels.getText(channelId),
-      deps.users.getSelf(authorId),
-    ]);
+        const [channel, author] = await Promise.all([
+            deps.channels.getText(channelId),
+            deps.users.getSelf(authorId),
+        ]);
 
-    var message = (await deps.messages.create(authorId, channelId, {
-      attachmentURLs,
-      content,
-      embed,
-    })) as Entity.Message;
+        var message = (await deps.messages.create(authorId, channelId, {
+            attachmentURLs,
+            content,
+            embed,
+        })) as Entity.Message;
+        console.log(message);
 
-    channel.lastMessageId = message.id;
-    await deps.channels.save(channel);
+        channel.lastMessageId = message.id;
+        await deps.channels.save(channel);
 
-    await deps.pings.markAsRead(author, message);
+        await deps.pings.markAsRead(author, message);
 
-    return [
-      {
-        emit: this.on,
-        to: [channelId],
-        send: { message } as WS.Args.MessageCreate,
-      }
-    ];
-  }
+        return [
+            {
+                emit: this.on,
+                to: [channelId],
+                send: { message } as WS.Args.MessageCreate,
+            },
+        ];
+    }
 }
